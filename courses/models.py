4662bfe7010bb1f89c378a5e7372af5853c4b913
Rafael R. Marquesi <marquesi.cc@gmail.com>
2018-07-09 21:26:41 -0400

    Initial commit

---
+++ courses/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+
+from django.conf import settings
+
+
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
+class Course(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Atalho')
+    description = models.TextField('Descrição Simples', blank=True)
+    about = models.TextField('Sobre o curso', blank=True)
+    start_date = models.DateField('Data de Início', null=True, blank=True)
+    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('courses:details', (), {'slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']
+
+class Enrollment(models.Model):
+
+    STATUS_CHOICES = (
+        (0, 'Pendente'),
+        (1, 'Aprovado'),
+        (2, 'Cancelado'),
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name='Usuário', on_delete=True,
+        related_name='enrollments'
+    )
+    course = models.ForeignKey(
+        Course, verbose_name='Curso', related_name='enrollments', on_delete=True
+    )
+    status = models.IntegerField(
+        'Situação', choices=STATUS_CHOICES, default=0, blank=True
+    )
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    def active(self):
+        self.status = 1
+        self.save()
+
+    def is_approved(self):
+        return self.status == 1
+
+    class Meta:
+        verbose_name = 'Inscrição'
+        verbose_name_plural = 'Inscrições'
+        unique_together = (('user', 'course'),)
+
+
+class Announcement(models.Model):
+    course = models.ForeignKey(
+        Course, verbose_name='Curso', related_name='announcements', on_delete=True
+    )
+    title = models.CharField('Título', max_length=100)
+    content = models.TextField('Conteúdo')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Anúncio'
+        verbose_name_plural = 'Anúncios'
+        ordering = ['-created_at']
+
+class Comment(models.Model):
+    announcement = models.ForeignKey(Announcement, verbose_name='Anúncio', related_name='comments', on_delete=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='usuário', on_delete=True)
+    comment = models.TextField('Comentário')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Comentário'
+        verbose_name_plural = 'Comentários'
+        ordering = ['created_at']
---
+++ courses/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Course, Enrollment
+from .forms import ContactCourse
+
+
+def index(request):
+    courses = Course.objects.all()
+    template_name = 'courses/index.html'
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)
+
+
+# def details(request, pk):
+#     course = get_object_or_404(Course, pk=pk)
+#     context = {
+#         'course': course
+#     }
+#     template_name = 'courses/details.html'
+#     return render(request, template_name, context)
+
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_mail(course)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['form'] = form
+    context['course'] = course
+    template_name = 'courses/details.html'
+    return render(request, template_name, context)
+
+
+@login_required
+def enrollment(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    enrollment, created = Enrollment.objects.get_or_create(
+        user=request.user, course=course
+    )
+    if created:
+        enrollment.active()
+        messages.success(request, 'Você foi inscrito no curso com sucesso')
+    else:
+        messages.info(request, 'Você já está inscrito no curso')
+
+    return redirect('accounts:dashboard')
+
+@login_required
+def undo_enrollment(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    enrollment = get_object_or_404(
+        Enrollment, user=request.user, course=course
+    )
+    if request.method == 'POST':
+        enrollment.delete()
+        messages.success(request, 'Sua inscrição foi cancelada com sucesso')
+        return redirect('accounts:dashboard')
+    template = 'courses/undo_enrollment.html'
+    context = {
+        'enrollment': enrollment,
+        'course': course
+    }
+    return render(request, template, context)
+
+@login_required
+def announcements(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    if not request.user.is_staff:
+        enrollment = get_object_or_404(
+            Enrollment, user=request.user, course=course
+        )
+        if not enrollment.is_approved():
+            messages.error(request, 'A sua inscrição está pendente')
+            return redirect('accounts:dashboard')
+    template = 'courses/announcements.html'
+    context = {
+        'course': course,
+        'announcements': course.announcements.all()
+    }
+    return render(request, template, context)
