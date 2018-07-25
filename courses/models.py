b4ad3c152aa83a9227c2da6de02d44a4c79bcf65
Rafael R. Marquesi <marquesi.cc@gmail.com>
2018-07-24 22:42:59 -0400

    Salvando anúncio e enviando e-mail

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.conf import settings
 
+from core.mail import send_mail_template
 
 class CourseManager(models.Manager):
 
@@ -100,3 +101,19 @@ class Comment(models.Model):
         verbose_name = 'Comentário'
         verbose_name_plural = 'Comentários'
         ordering = ['created_at']
+
+def post_save_announcement(instance, created, **kwargs):
+    if created:
+        subject = instance.title
+        context = {
+            'announcement': instance
+        }
+        template_name = 'courses/announcement_mail.html'
+        enrollments = Enrollment.objects.filter(course=instance.course, status=1)
+        for enrollment in enrollments:
+            recipient_list = [enrollment.user.email]
+            send_mail_template(subject, template_name, context, recipient_list)
+
+models.signals.post_save.connect(
+    post_save_announcement, sender=Announcement, dispatch_uid='post_save_announcement'
+)
